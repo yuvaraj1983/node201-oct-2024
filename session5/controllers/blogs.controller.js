b0a66c4b61c8e83f33fcde8d5bf3e6cefc4647f9
yuvaraj <yuvaraj@coursera-something.com>
2025-03-06 00:07:30 +0530

    session5 completed

--- session5/controllers/blogs.controller.js
+++ session5/controllers/blogs.controller.js
@@ -5,8 +5,12 @@ const createNewBlog = async (req, res) => {
     //console.log(req.body);
     try {
         const newBlog = await Blog.create(req.body);
-        res.send(newBlog);
+        res.status(201).send(newBlog);
     } catch (error) {
+        if(error.name === "ValidationError")
+            return res.status(400).send({message: error.message});
+        if(error.code === 11000)
+            return res.status(409).send({message: "Blog with this title already exists."})
         res.status(500).send({ message: `Something went wrong ${error.message}`});
     }
     
@@ -30,12 +34,36 @@ const getBlogById = async (req, res) => {
         res.status(404).send({message: `Blog with Id: ${blogId} not found`});
     } catch (error) {
         console.log(error.name);
-        if(error.name === "CastError")
-           return res.status(400).send({message: `Invalid blog id: ${blogId}`})
+        // if(error.name === "CastError")
+        //    return res.status(400).send({message: `Invalid blog id: ${blogId}`})
         res.status(500).send({ message: `Something went wrong ${error.message}`});
     }
     
 
 }
 
-module.exports = { createNewBlog, getAllBlogs, getBlogById }
+const updateBlogById = async (req, res) => {
+    const { blogId } = req.params;
+    try {
+        const updatedBlog = await Blog.findByIdAndUpdate(blogId, req.body, { new: true,});
+        if(!updatedBlog) return res.status(404).send({message: `Blog with id ${blogId} not found`})
+        return res.send(updatedBlog);
+    } catch (error) {
+        res.status(500).send({ message: `Something went wrong ${error.message}`});
+    }
+
+}
+
+const deleteBlogById = async (req, res) => {
+    const { blogId } = req.params;
+    const deletedBlog = await Blog.findByIdAndDelete(blogId);
+    if(!deletedBlog) return res.status(404).send({message: `Blog with id ${blogId} not found`})
+    return res.sendStatus(204);
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+module.exports = { createNewBlog, getAllBlogs, getBlogById, updateBlogById, deleteBlogById }
