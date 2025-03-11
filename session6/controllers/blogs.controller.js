bd4c5df2715c85b1c7ac35d0c117cc4d330c752f
yuvaraj <yuvaraj@coursera-something.com>
2025-03-11 23:56:46 +0530

    mongodb queries

--- session6/controllers/blogs.controller.js
+++ session6/controllers/blogs.controller.js
@@ -1,3 +1,4 @@
+const { default: isEmail } = require("validator/lib/isEmail");
 const Blog = require("../models/blog.model");
 
 
@@ -67,4 +68,69 @@ const deleteBlogById = async (req, res) => {
     }
 }
 
-module.exports = { createNewBlog, getAllBlogs, getBlogById, updateBlogById, deleteBlogById }
+// const searchBlogs = async (req, res) => {
+//     //console.log(req.query);
+//     const { title, author } = req.query;
+//     // const titleRegex = new RegExp(title, "i");
+    
+//     // res.send(
+//     //     await Blog.find({ title: titleRegex})
+//     // );
+
+//     // const titleRegex = new RegExp(title, "i"); //pattern contains the regex option
+//     // const titleQuery = { title: { $regex: titleRegex } }
+
+//     // const titleRegex = new RegExp(title); //pattern doesn't contain the regex option
+//     // const titleQuery = { title: { $regex: titleRegex, $options: 'i' } }
+  
+//     // res.send(await Blog.find(titleQuery));
+
+//     const authorRegex = new RegExp(author);
+//     const authorNameQuery = { 
+//         author: { 
+//             $elemMatch : {
+//                  fullName : { $regex: authorRegex, $options: "i"} }, 
+//             }
+//         }
+
+//         const authorRegex = new RegExp(author);
+//         const authorEmailQuery = { 
+//             author: { 
+//                 $elemMatch : {
+//                      //fullName : { $regex: authorRegex, $options: "i"} }, 
+//                      email: author
+//                 }
+//             }
+//         }  
+//     }
+
+const searchBlogs = async (req, res) => {
+    const { title, author } = req.query;
+
+    const titleRegex = new RegExp(title); //pattern doesn't contain the regex option
+    const titleQuery = { 
+        title: { $regex: titleRegex, $options: 'i' } 
+    };
+
+    const authorQuery = { 
+            author: { 
+                $elemMatch : { email: author }
+            },
+    }; 
+
+    if(title && author) {
+        return res.send(await Blog.find({ $and: [ titleQuery, authorQuery ]}));
+    } else if(title) {
+        return res.send(await Blog.find(titleQuery));
+    } else if(author){
+        return res.send(await Blog.find(authorQuery));
+    } else {
+        res.status(400).send({message: `At least one of 'title' or 'author' is needed to search blogs!`,});
+    }
+
+}
+    
+
+module.exports = { createNewBlog, getAllBlogs, getBlogById, updateBlogById, deleteBlogById,
+    searchBlogs
+ }
--- session6/routes/blogs.routes.js
+++ session6/routes/blogs.routes.js
@@ -1,10 +1,14 @@
 const router = require("express").Router();
-const { createNewBlog, getAllBlogs, getBlogById, updateBlogById, deleteBlogById } = require("../controllers/blogs.controller")
+const { createNewBlog, getAllBlogs, getBlogById, updateBlogById, deleteBlogById,
+    searchBlogs
+
+ } = require("../controllers/blogs.controller")
 const pathValidator = require("../middleware/pathValidator");
 const { blogIdSchema } = require("../validations/blogs.validation");
 
 router.post("/new", createNewBlog);
 router.get("/", getAllBlogs);
+router.get("/search", searchBlogs);
 
 // router.get("/:blogId", getBlogById);
 // router.patch("/:blogId", updateBlogById);
